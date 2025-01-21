48d069886eb05c217d43300c598cb12317f5a80d
dantegld <dantegallardoledon@gmail.com>
2025-01-21 14:29:52 +0100

    header footer

--- website/home.php
+++ website/home.php
@@ -13,56 +13,16 @@
 </head>
 <body>
 <?php
-include 'functies/sideMenu.php';
-
+include 'functies/pagina/header.php';
 ?>
 
 <main>
     <h2>Welkom bij Site Solutions</h2>
 </main>
 
-     
-    <footer>
-  <div class="footer-container">
-    <div class="footer-section">
-      <h3>Openingstijden</h3>
-      <ul>
-        <li>Maandag - Zondag: <br>24/7</li>
-          
-      </ul>
-    </div>
-
-    <div class="footer-section">
-      <h3>Volg ons</h3>
-      <ul>
-        <li><a href="https://instagram.com/sitesolutionsbe" target="_blank"><img src="images/insta.png" width="50px" height="50px"></a></li>
-      </ul>
-    </div>
-
-    <div class="footer-section">
-      <h3>Nieuwsbrief</h3>
-      <form class="center-container" action="#">
-        <input type="email" placeholder="Voer je e-mail in" required>
-        <button type="submit">Inschrijven</button>
-      </form>
-    </div>
-
-    <div class="footer-section">
-      <h3>Locatie</h3>
-      <div style="width: 100%"><iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Gustave%20van%20de%20Woestijnestraat%2052+(Site%20Solutions)&amp;t=&amp;z=16&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps trackers</a></iframe></div>
-    </div>
-
-    <div class="footer-section">
-      <h3>Contact</h3>
-      <ul>
-        <li><a href="mailto:sitesolutions.contact@gmail.com">sitesolutions.contact@gmail.com</a></li>
-        <li><a href="tel:+32499912181">+32 499 91 21 81</a></li>
-        <li><a href="tel:+32478102541">+32 478 10 25 41</a></li>
-        <li><a href="tel:+32471041591">+32 471 04 15 91</a></li>
-      </ul>
-    </div>
-  </div>
-</footer>
+<?php
+include 'functies/pagina/footer.php';
+?>
 
 <script>
     document.addEventListener('DOMContentLoaded', function() {
